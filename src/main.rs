09aafe98b09adca266bbc28059afd95800b5716f
isso0424 <isso04240129@gmail.com>
2020-07-06 01:45:54 +0900

    [Fix]Simplification file creating function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
 
-use crate::command::types::{label::Labels, task::Tasks};
+use crate::command::types::task::Tasks;
 #[macro_use]
 extern crate log;
 
@@ -55,31 +55,32 @@ fn load_args() -> Result<Vec<String>, String> {
     Ok(args)
 }
 
-fn create_json() -> Result<(), std::io::Error> {
-    let task_dir_path = current_exe()?.parent().unwrap().join("tasks.json");
+fn create_file(file_name: &str) -> Result<(), std::io::Error> {
+    let path = current_exe()?.parent().unwrap().join(file_name);
 
-    if !task_dir_path.is_file() {
-        let file = File::create(task_dir_path)?;
-        let mut writer = BufWriter::new(file);
-        let empty_tasks = Tasks { content: vec![] };
-
-        write!(writer, "{}", serde_json::to_string(&empty_tasks)?)?;
+    if path.is_file() {
+        return Ok(());
     }
 
-    let label_dir_path = current_exe()?.parent().unwrap().join("labels.json");
+    let file = File::create(path)?;
+    let mut writer = BufWriter::new(file);
+    let empty_tasks = Tasks { content: vec![] };
 
-    if !label_dir_path.is_file() {
-        let file = File::create(label_dir_path)?;
-        let mut writer = BufWriter::new(file);
-        let empty_labels = Labels { content: vec![] };
+    write!(writer, "{}", serde_json::to_string(&empty_tasks)?)?;
 
-        write!(writer, "{}", serde_json::to_string(&empty_labels)?)?;
-    }
+    Ok(())
+}
+
+fn create_data_json() -> Result<(), std::io::Error> {
+    create_file("tasks.json")?;
+    create_file("labels.json")?;
 
     Ok(())
 }
 
 fn execute(args: Vec<String>) -> Result<(), String> {
+    create_data_json().map_err(|err| err.to_string())?;
+
     let command_name = &args[1];
 
     match command_name.as_str() {
@@ -107,9 +108,9 @@ fn main() {
             std::process::exit(1);
         }
     };
-    let _ = create_json().map_err(|x| error!("{}", x));
+
     match execute(args) {
-        Ok(_) => print!("ok"),
+        Ok(_) => print!("executed!!!"),
         Err(error) => print!("{}", error),
     }
 }
