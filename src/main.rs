3cdfe0044f2b58b8adc35b026a59d8d2b41b78e6
isso0424 <isso04240129@gmail.com>
2020-07-15 02:16:26 +0900

    [Add]write check_args() test failed & success

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,9 @@ extern crate log;
 mod command;
 mod config;
 
-fn check_args() -> Result<Vec<String>, String> {
+fn check_args(args: Vec<String>) -> Result<Vec<String>, String> {
     let not_enough_args: String = "Not enough args".to_string();
 
-    let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         return Err(not_enough_args);
     }
@@ -84,7 +83,8 @@ fn execute(args: Vec<String>) -> Result<(), String> {
 }
 
 fn main() {
-    let args = match check_args() {
+    let raw_args: Vec<String> = env::args().collect();
+    let args = match check_args(raw_args) {
         Ok(arg) => arg,
         Err(error) => {
             error!("{}", error);
@@ -95,5 +95,69 @@ fn main() {
     if let Err(error) = execute(args) {
         error!("{}", error);
         std::process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_args_success() {
+        let commands: Vec<String> = vec!["check", "done", "create", "update", "delete"]
+            .iter()
+            .map(|arg| arg.to_string())
+            .collect();
+
+        let mut args: Vec<String> = vec!["", "command", "hoge"]
+            .iter()
+            .map(|arg| arg.to_string())
+            .collect();
+
+        let switch_timing = 1;
+        let mut index = 0;
+
+        for command in commands {
+            if switch_timing == index {
+                args.push("fuga".to_string())
+            }
+            args[1] = command;
+            assert_eq!(check_args(args.clone()), Ok(args.clone()));
+            index += 1;
+        }
+    }
+
+    #[test]
+    fn check_args_failed() {
+        let not_enough_args: String = "Not enough args".to_string();
+
+        let commands: Vec<String> = vec!["check", "done", "create", "update", "delete"]
+            .iter()
+            .map(|arg| arg.to_string())
+            .collect();
+
+        let mut args: Vec<String> = vec!["", "command"]
+            .iter()
+            .map(|arg| arg.to_string())
+            .collect();
+
+        let switch_timing = 1;
+        let mut index = 0;
+
+        for command in commands {
+            args[1] = command;
+            assert_eq!(check_args(args.clone()), Err(not_enough_args.clone()));
+            if switch_timing == index {
+                args.push("fuga".to_string())
+            }
+            index += 1;
+        }
+
+        args[1] = "failed".to_string();
+
+        assert_eq!(
+            check_args(args.clone()),
+            Err("Invalid subcommand".to_string())
+        );
     }
 }
