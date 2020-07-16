584c2b3c9f0d65b30980bc4eb920120a5fb08bb7
isso0424 <isso04240129@gmail.com>
2020-07-16 23:11:04 +0900

    [Split]Delete split to delete and update

--- src/command/delete.rs
+++ src/command/delete.rs
@@ -1,37 +1,50 @@
 use crate::command::types::{label::Labels, task::Tasks};
 
-fn delete_task(title: String) -> Result<(), String> {
-    let mut tasks = Tasks::load().map_err(|err| err.to_string())?;
-
+fn update_task(mut tasks: Tasks, title: String) -> Result<Tasks, String> {
     if !tasks.content.iter().any(|task| task.title == title) {
         return Err("Task not found".to_string());
     }
 
-    let new_tasks = tasks
-        .content
-        .drain(..)
-        .filter(|task| task.title != title)
-        .collect();
+    Ok(Tasks {
+        content: tasks
+            .content
+            .drain(..)
+            .filter(|task| task.title != title)
+            .collect(),
+    })
+}
 
-    tasks.content = new_tasks;
+fn delete_task(title: String) -> Result<(), String> {
+    let tasks = match Tasks::load() {
+        Ok(tasks) => update_task(tasks, title)?,
+        Err(err) => return Err(err.to_string()),
+    };
+
     tasks.save().map_err(|err| err.to_string())?;
 
     Ok(())
 }
 
-fn delete_label(title: String) -> Result<(), String> {
-    let mut labels = Labels::load().map_err(|err| err.to_string())?;
-
+fn update_label(mut labels: Labels, title: String) -> Result<Labels, String> {
     if !labels.content.iter().any(|label| label.title == title) {
         return Err("Label not found".to_string());
     }
 
-    let new_labels = labels
-        .content
-        .drain(..)
-        .filter(|label| label.title != title)
-        .collect();
-    labels.content = new_labels;
+    Ok(Labels {
+        content: labels
+            .content
+            .drain(..)
+            .filter(|label| label.title != title)
+            .collect(),
+    })
+}
+
+fn delete_label(title: String) -> Result<(), String> {
+    let labels = match Labels::load() {
+        Ok(labels) => update_label(labels, title)?,
+        Err(err) => return Err(err.to_string()),
+    };
+
     labels.save().map_err(|err| err.to_string())?;
 
     Ok(())
